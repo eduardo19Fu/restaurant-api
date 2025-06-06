9c025587ecba4291820d41829d9e60208abbc672
Eduardo Fuentes <eduardo.fuentes@viaro.net>
2025-06-06 10:24:40 -0600

    Manejo de errores agregado

---
+++ src/main/java/com/pangosoft/restaurant/error/exceptions/NoContentException.java
@@ -0,0 +1,7 @@
+package com.pangosoft.restaurant.error.exceptions;
+
+public class NoContentException extends RuntimeException {
+    public NoContentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/pangosoft/restaurant/error/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.pangosoft.restaurant.error.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
